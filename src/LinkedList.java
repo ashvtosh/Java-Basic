0dbdeab1febdc7bf7c4833ba341901f01c7cd804
ashu <ashutosh1412003@gmail.com>
2022-02-04 01:25:44 +0530

    hello

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -19,12 +19,12 @@ class LinkedList {
     }
     public static void main(String[] args)
     {
-        LinkedList llist = new LinkedList();
-        llist.head = new Node(1);
+        LinkedList a = new LinkedList();
+        a.head = new Node(1);
         Node second = new Node(2);
         Node third = new Node(3);
-        llist.head.next = second;
+        a.head.next = second;
         second.next = third;
-        llist.printList();
+        a.printList();
     }
 }
